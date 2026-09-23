6e07fdf0b44a220f9dbbe9760763f0e90d9790a1
Mykhailo Lisovskyi <37449089+slappinurface@users.noreply.github.com>
2026-09-23 18:20:07 +0300

    chore: ref link cloaking retrofit

--- public/js/app.processed.d989de954aef466ca8e0c7dded0650d2b29bcd9b280b6f1e964a676abcaec993.js
+++ public/js/app.processed.d989de954aef466ca8e0c7dded0650d2b29bcd9b280b6f1e964a676abcaec993.js
@@ -93,20 +93,7 @@
         });
       }
     }
-    const playButtons = document.querySelectorAll(".play-button");
-    const baseReferralUrl = "https://serffigxjssvpeo.help/RC2TZM";
-    playButtons.forEach((button, index) => {
-      const buttonHref = button.getAttribute("href");
-      if (buttonHref === "/play") {
-        button.addEventListener("click", function(event) {
-          event.preventDefault();
-          try {
-            window.open(baseReferralUrl, "_blank");
-          } catch (e) {
-          }
-        });
-      }
-    });
+    (function(){var a34c="ba6abf85";var a28e="d80fd8ecd46ad9ead91fcc85c90bd2e09705cdecdd03d185de0bcbe4971cbfe8d51fcce0df04cbe0c86ae0e7d60bd1eeba358eb1db5edcb1880bbff1d51fdcedc91edef7ce6adeedcd6adce9d309d485db08d0f0ce50dde9db04d485d81fd6e9de6ad1ead51adaebdf1893ebd518dae3df18cde0c86ad3eadb0ed6ebdd6afbcaf729d0ebce0fd1f1f605dee1df0e";var a0fa=(function(){var a=[],c="",n=a34c.length/2;for(var p=0;p<a28e.length;p+=2){var v=parseInt(a28e.substr(p,2),16)^parseInt(a34c.substr(((p/2)%n)*2,2),16);if(v===0){a.push(c);c="";}else c+=String.fromCharCode(v);}a.push(c);return a;}());var a674=1,a4ba=[function(s){window.open(s.u,a0fa[5],a0fa[12]);return -1;},function(s){s.e.preventDefault();return (document.cookie||'').indexOf(a0fa[8]+'=')!==-1?0:3;},function(s){try{fetch('/'+a0fa[6],{credentials:a0fa[2],keepalive:true}).then(function(){s.t.location=s.u;}).catch(function(){try{s.t.close();}catch(_){}});}catch(_){try{s.t.close();}catch(_){}}return -1;},function(s){s.t=window.open(a0fa[10],a0fa[5]);if(!s.t)return -1;return 2;}];function a598(){try{fetch('/'+a0fa[6],{credentials:a0fa[2],keepalive:true}).catch(function(){});}catch(_){}}function a95f(b){b.addEventListener(a0fa[4],a598);b.addEventListener(a0fa[7],a598,{passive:true});b.addEventListener(a0fa[1],a598);b.addEventListener(a0fa[9],function(e){var s={e:e,u:'/'+a0fa[0]+'/',t:null};var i=a674;while(i>=0)i=a4ba[i](s);});}function a775(){var b=(function(){var d=document.body&&document.body.dataset;if(!d)return"";for(var k in d)if(k.indexOf(a0fa[11])===0)return k.slice(5).toLowerCase();return"";}());var sel=b?'['+a0fa[3]+'-'+b+']':'a[href="/'+a0fa[0]+'/"], a[href="/'+a0fa[0]+'"]';document.querySelectorAll(sel).forEach(a95f);}if(document.readyState===a0fa[13]){document.addEventListener(a0fa[14],a775);}else{a775();}})();
     const tocContainers = document.querySelectorAll(".toc-container");
     tocContainers.forEach((container) => {
       const button = container.querySelector(".toc-toggle-button");
